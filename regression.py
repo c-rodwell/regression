a22c38ce0d86b078dc88fabc31ff9aaed0b8bcdf
Colin <rodwell.colin@gmail.com>
2020-07-14 01:38:40 -0700

    fix issues with multi-target regression

--- regression.py
+++ regression.py
@@ -10,7 +10,7 @@ INITIAL_ETA = 0.03 #0.03 and lower works, but 0.1 too big -> weights go to large
 ETA_DIVISION = 10
 EPOCH_LENGTH = 1000
 NUM_EPOCHS = 10
-NUMERICAL_GRAD_DELTA = pow(10,-5)#pow(10,-15)
+NUMERICAL_GRAD_DELTA = pow(10,-10)#pow(10,-15)
 
 # def load():
 # 	features = np.array(pd.read_csv(INPUT_PATH, usecols = [i for i in range(NUM_FEATURES)]))
@@ -31,12 +31,14 @@ def to_1d_vec(vec):
 def predict_linear_single(point, weights):
 	return np.dot(point, weights)
 
-#predict all the points. 
+#predict all the points.
+#dot product of point and weight generalizes to matrix multiplication of points vector with weights
+#this also works when weights has multiple columns for multiple features
 def predict_linear_all(points, weights):
 	return np.matmul(points, weights)
 
 #pick the best model weights for the data
-def train_gradient_descent(features, labels, error_func, grad_func, initial_eta, eta_divide, epoch_length, num_epochs):
+def train_gradient_descent(features, labels, error_func, grad_func, initial_eta, eta_divide, epoch_length, num_epochs, grad_to_compare=None):
 	#set initial weights- all 0, all 1 or something like that
 	#loop over num_epochs:
 		#loop over epoch length
@@ -47,7 +49,13 @@ def train_gradient_descent(features, labels, error_func, grad_func, initial_eta,
 		#set learning rate = previous / eta_divide
 	#return weights
 
-	weights = np.zeros((NUM_FEATURES+1, 1)) #initialize weights at 0 - does it matter where we start?
+	#weights = np.zeros((NUM_FEATURES+1, 1)) #initialize weights at 0 - does it matter where we start?
+
+	#weights matri: map dimensions of one point(3 with augmented) to dimensions of label(2)
+	label_dimension = labels.shape[1]
+	feature_dimension = features.shape[1]
+	weights = np.zeros((feature_dimension, label_dimension))
+
 	eta = initial_eta
 	for epoch in range(num_epochs):
 		
@@ -66,9 +74,9 @@ def train_gradient_descent(features, labels, error_func, grad_func, initial_eta,
 		print("\tweights after update:\n"+str(weights))
 		print("\tloss after update: "+str(loss))
 
-		#compare different grad methods:
-		print("\nend of epoch gradient based on calculated formula:\n"+str(mean_squared_gradient(features, labels, weights)))
-		print("\nend of epoch gradient from numerical method:\n"+str(numerical_gradient(mean_squared_loss_weights, features, labels, weights)))
+		if grad_to_compare:
+			print("\nend of epoch gradient based on current gradient "+str(grad_func)+":\n"+str(grad_func(features, labels, weights)))
+			print("\nend of epoch gradient based on other gradient "+str(grad_to_compare)+":\n"+str(grad_to_compare(features, labels, weights)))
 
 	return weights
 
@@ -100,14 +108,62 @@ def numerical_gradient_of(lossfunc):
 #must be a lossfunc form with weight argument like mean_squared_loss_weights(features, labels, weights). not mean_squared_loss(predictions, labels)
 def numerical_gradient(lossfunc, features, labels, weights):
 	delta = NUMERICAL_GRAD_DELTA 
-	grad = np.zeros(weights.shape)
+	grad = np.ones(weights.shape)
 	loss_base = lossfunc(features, labels, weights)
-	for i in range(weights.size):
-		#take derivate with respect to weight[i]
-		adjusted_weights = weights.copy()
-		adjusted_weights[i] += delta
-		grad[i] = (lossfunc(features, labels, adjusted_weights) - loss_base) / delta
+
+	for i in range(weights.shape[0]):
+		for j in range(weights.shape[1]):
+			#take derivate with respect to weight[i]
+			adjusted_weights = weights.copy()
+			adjusted_weights[i][j] += delta
+			value = (lossfunc(features, labels, adjusted_weights) - loss_base) / delta
+			#print("value is "+str(value))
+			#grad[i][j] = 1
+			grad[i][j] = value
 	return grad
+
+#multiple targets: now weights has 2 dimensions
+#can still use same prediction function, but loss is different
+def multi_target_mean_squared_loss_weights(features, labels, weights):
+	predictions = predict_linear_all(features, weights)
+	return multi_target_mean_squared_loss(predictions, labels)
+
+def multi_target_mean_squared_loss(predictions, labels):	
+	if predictions.shape != labels.shape:
+		raise ValueError("number or dimensionality of predictions does not match number of labels")
+	n = len(predictions)
+	errors = predictions - labels
+	return sum([length_squared(predictions[i] - labels[i]) for i in range(n)]) / (2*n)
+
+#gradient for multi target means squared loss with respect to weights.
+#derivatives separate between the different target labels, so this is just a combination of the single target gradient for each target.
+def multi_target_mean_squared_gradient(features, labels, weights):
+	num_feature_vecs = features.shape[0]
+	num_label_vecs = labels.shape[0]
+	num_features = features.shape[1]
+	num_labels = labels.shape[1]
+
+	if num_feature_vecs	 != num_label_vecs	:
+		raise ValueError("number of features and labels doesnt match")
+	if weights.shape[0] != num_features:
+		raise ValueError("weights dim 0 doesn't match num features")
+	if weights.shape[1] != num_labels:
+		raise ValueError("weights dim 1 doesn't match num labels")
+
+	grad = np.zeros(weights.shape)
+	for i in range(num_labels):
+		#do part of gradient for ith label:
+		#label vector is ith column of features
+		#weights are ith column of weights only
+		#features is entire feature matrix
+		#gradient result is ith column of total gradient (matches shape of weights)
+		grad[:,i] = mean_squared_gradient(features, labels[:,i], weights[:,i])
+	return grad
+
+
+#compute squared magnitude of a vector which is in 1d form
+def length_squared(vec):
+	return np.dot(vec, vec)
 
 #using some fraction of data (randomly selected) as training set:
 #train the model, test on remaining data, report statistics
@@ -141,7 +197,7 @@ def single_test_train(train_fraction, loss, grad):
 #regression where target is two numbers - loss will be sum of squared vector differences
 #use the first 2 features as features, second 2 as labels
 #gradient and error functions must now handle Y of shape (N,2) instead of (N,1)
-def two_numerical_targets_regression(train_fraction, loss, grad):
+def two_numerical_targets_regression(train_fraction, loss, grad, compare_grad=None):
 	fulldata = pd.read_csv(INPUT_PATH, header=None)
 	train_data = fulldata.sample(frac=train_fraction)
 	test_data = fulldata.drop(train_data.index)
@@ -152,7 +208,7 @@ def two_numerical_targets_regression(train_fraction, loss, grad):
 	#train, then test
 	#should predicting and computing loss be combined? both involve predicting.
 
-	weights = train_gradient_descent(train_X_aug, train_Y, loss, grad, INITIAL_ETA, ETA_DIVISION, EPOCH_LENGTH, NUM_EPOCHS)
+	weights = train_gradient_descent(train_X_aug, train_Y, loss, grad, INITIAL_ETA, ETA_DIVISION, EPOCH_LENGTH, NUM_EPOCHS, compare_grad)
 	predictions_train = predict_linear_all(train_X_aug, weights)
 
 	train_loss = loss(train_X_aug, train_Y, weights)
@@ -165,7 +221,6 @@ def two_numerical_targets_regression(train_fraction, loss, grad):
 	print("loss on test set: "+str(test_loss))
 	print("truth - prediction")
 	print(np.concatenate((test_Y, predictions_test), axis = 1))
-
 
 #whatever statistics we want: accuracy etc 
 def statistics():
@@ -207,8 +262,8 @@ def divide_2_feature_2_label(data):
 	X_aug = augment(X)
 	return (X_aug, Y)
 
-
 def main():
 	#single_test_train(0.7, mean_squared_loss_weights,numerical_gradient_of(mean_squared_loss_weights))
 	#single_test_train(0.7, mean_squared_loss_weights, mean_squared_gradient)
-	two_numerical_targets_regression(0.7, mean_squared_loss_weights, mean_squared_gradient)
+	#two_numerical_targets_regression(0.7, multi_target_mean_squared_loss_weights, numerical_gradient_of(multi_target_mean_squared_loss_weights))
+	two_numerical_targets_regression(0.7, multi_target_mean_squared_loss_weights, multi_target_mean_squared_gradient, compare_grad = numerical_gradient_of(multi_target_mean_squared_loss_weights))
